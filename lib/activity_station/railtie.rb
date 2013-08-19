a914e2ecb3c5acc9a01f51bd783366a9264dd203
Yves Senn <yves.senn@gmail.com>
2013-08-19 09:22:58 +0200

    Allow config/initializers to setup the env.

--- lib/activity_station/railtie.rb
+++ lib/activity_station/railtie.rb
@@ -2,7 +2,7 @@ require 'rails/railtie'
 
 module ActivityStation
   class Railtie < ::Rails::Railtie
-    initializer "activity_station.configure" do
+    initializer "activity_station.configure", after: :load_config_initializers do
       provider = ActivityStation.configure(Rails.env)
       config.activity_station = provider
     end
